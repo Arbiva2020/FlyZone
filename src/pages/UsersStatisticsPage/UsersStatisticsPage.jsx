08d4cff28586a3688134f67b33e4a4b34d61dc44
Adi Vaknin <arbiva0@gmail.com>
2023-04-27 00:38:50 +0300

    installing react chart

--- src/pages/UsersStatisticsPage/UsersStatisticsPage.jsx
+++ src/pages/UsersStatisticsPage/UsersStatisticsPage.jsx
@@ -2,8 +2,42 @@ import React from 'react'
 import AuthHeader from '../../components/AuthHeader/AuthHeader'
 import SideBar from '../../components/SideBar/SideBar'
 import "./UsersStatisticsPage.css"
+import fakeData from '../../fakeData.json'
+import { useTable } from "react-table"
 
 const UsersStatisticsPage = () => {
+  const data = React.useMemo(() => fakeData, []);
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "First Name", 
+        accessor: 'firstName'
+      }, 
+      {
+        Header: "Last Name", 
+        accessor: 'lastName'
+      }, 
+      {
+        Header: "Level", 
+        accessor: 'currentLevel'
+      }, 
+      {
+        Header: "MMR", 
+        accessor: 'MMR'
+      }, 
+      {
+        Header: "No. of Badges", 
+        accessor: 'badges'
+      }, 
+      {
+        Header: "Total Score", 
+        accessor: 'totalScore'
+      }, 
+    ], []
+  );
+
+  const {getTableProps, getTableBodyProps, headerGroups, rows, prepareRow} = useTable({columns, data})
+console.log(fakeData)
   return (
     <div>
       <div className='users_main'>
@@ -14,7 +48,33 @@ const UsersStatisticsPage = () => {
         </div>
           <div className='users_data'>
             <div className='users_headline'>Users Stats</div>
-            <div className='users_chart'>chart</div>
+            <div className='users_chart'>
+              <table {...getTableProps()}>
+                <thead>
+                  {headerGroups.map((headerGroup) => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                      {headerGroup.headers.map((column) => (
+                        <th {...column.getHeaderProps()}>
+                          {column.render("Header")}
+                        </th>
+                      ))}
+                    </tr>
+                  ))}
+                </thead>
+                <tbody {...getTableBodyProps()}>
+                  {rows.map((row) => {
+                    prepareRow(row)
+                    return (
+                      <tr {...row.getRowProps()}>
+                        {row.cells.map((cell) => (
+                          <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                        ))}
+                      </tr>
+                    )
+                  })}
+                </tbody>
+              </table>
+            </div>
         </div>
         <div className='users_summary'>
             <p className='users_summary_total'>Total users: {}</p>
